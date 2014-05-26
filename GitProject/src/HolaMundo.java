e6339f18fa5999de76fc9bb1efdddb086b9e7a09
Paco Gomez <fjgomez@florida-uni.es>
2014-05-26 12:05:57 +0200

    Imprimo integer
    Signed-off-by: Paco Gomez <fjgomez@florida-uni.es>

--- GitProject/src/HolaMundo.java
+++ GitProject/src/HolaMundo.java
@@ -1,9 +1,14 @@
 
 public class HolaMundo {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int contador=0;
+		
 		System.out.println("Hola compañeros");
+		System.out.println("Contador="+contador);
 	}
 
 }
